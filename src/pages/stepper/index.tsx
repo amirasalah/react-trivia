cf20973d3e1e0c7909c9877b34995c485eccd896
amira.dh <amera.salah.89@gmail.com>
2022-04-14 17:58:49 +0200

    Removing errors

--- src/pages/stepper/index.tsx
+++ src/pages/stepper/index.tsx
@@ -1,11 +1,13 @@
+/* eslint-disable unicorn/no-null */
 import React from "react";
 import useGetAllQuestions from "../../hooks/useGetAllQuestions";
 
 const Stepper: React.FC = () => {
   const { data, isLoading, isError } = useGetAllQuestions();
-  console.log(data);
 
-  return <div>Stepper</div>;
+  if (isLoading || isError) return null;
+
+  return <pre>{JSON.stringify(data, null, 4)}</pre>;
 };
 
 export default Stepper;
